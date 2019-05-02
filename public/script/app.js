0bee372209949468a8317a45464c213f29d350b9
YIYAO <jianyy1990@hotmail.com>
2019-05-02 15:01:50 +0000

    add attr for each td

--- public/script/app.js
+++ public/script/app.js
@@ -5,9 +5,22 @@ $(document).ready(function () {
     launchHit()
 })
 function launchHit() {
-    for (i = 1; i <= 10; i++) {
-        $('td').on('click', () => {
-            $('td').attr('foo', true);
-        })
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            $(`tr:nth-child(${i}) td:nth-child(${j})`).attr('location', a[i - 1][j - 1])
+            $(`tr:nth-child(${i}) td:nth-child(${j})`).attr('XY', `${i}${j}`)
+            $(`tr:nth-child(${i}) td:nth-child(${j})`).click(function () {
+                $(this).attr('foo', true)
+            })
+        }
+
     }
+
 }
+function hit() {
+
+}
+
+
+let a = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    , [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
